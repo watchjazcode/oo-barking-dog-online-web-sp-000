8a7bb48470fa018974948e9bd25e4a56f107bd27
Jazmin M <jazminmerckel@gmail.com>
2019-10-10 01:09:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
 # Your code goes here!
-class Dog 
-  def name=
+class Dog
+ 
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+ 
+  def name
+    @this_dogs_name
+  end
+end
+ 
+
+ 
